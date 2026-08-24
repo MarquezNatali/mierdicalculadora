e9c4accef9204afb17b5d6d778bf5175d98caeaf
MarquezNatali <natalimarquz08@gmail.com>
2026-08-24 09:40:53 -0300

    me equivoco escribiendo commits cmo no m voy a equivocar con el codigo

---
+++ triangulo.java
@@ -0,0 +1,14 @@
+public class triangulo extends Figuras{
+
+    @Override
+    public double calcularArea() {
+
+        return 0;
+    }
+
+    @Override
+    public double calcularPerimetro() {
+ 
+        return 0;
+    }
+}
